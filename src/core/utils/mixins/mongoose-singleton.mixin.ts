33d17310bd563bfc5853e0ab2157d9a9430b7345
Olex <alex99fil@gmail.com>
2020-04-20 23:12:50 +0300

    [REFACTORING] - TS - Mongoose abstract model  refactoring

--- src/core/utils/mixins/mongoose-singleton.mixin.ts
+++ src/core/utils/mixins/mongoose-singleton.mixin.ts
@@ -1,19 +1,31 @@
 import { Schema, model, Model, Document } from 'mongoose';
 
-export default function MongooseSingleton<T extends {new (...args: any[]): any;}>(originalConstructor: T): T & {getMongooseModel<U extends Document>(): Model<U>} {
+export default function MongooseSingleton<T extends {new (...args: any[]): any;}>(originalConstructor: T): T & {getMongooseModel<U extends Document>(settings?:  { prepopulate?: boolean }): Model<U>} {
     return class extends originalConstructor {
         private static mongooseModel;
-
+        
+        /**
+         * Create new mongoose model from schema
+         */
         private static init() { 
             const schema: Schema = new originalConstructor().schema;
             this.mongooseModel = model<Document>(originalConstructor.name, schema);
             return this.mongooseModel;
         }
 
-        public static getMongooseModel<U extends Document>(...args: any[]): Model<U> {
-            return this.mongooseModel || this.init();
+        /**
+         * Create or return (if exist) mongoose model
+         * @param settings - settings of generating mongoose model
+         */
+        public static getMongooseModel<U extends Document>(settings?: { prepopulate?: boolean }): Model<U> {
+            const model = this.mongooseModel || this.init();
+            if (settings?.prepopulate) {
+                model.save();
+            }
+            return model;
         }
 
+        // Constructor can not be called
         private constructor(...args: any[]) {
             super(...args);
         }
